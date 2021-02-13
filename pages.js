204482c35f4504c1533e0164433a843138ad2f64
Kajetan Knopp <kknopp@staszic.waw.pl>
2021-02-13 08:06:59 +0100

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "git@github.com:khknopp/khknopp.github.io.git";
 
 ghpages.publish(
   pathname,
